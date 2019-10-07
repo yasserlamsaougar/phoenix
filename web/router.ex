df8203e5eabbc162260240ebd7f34cb89671bd42
Lamsaougar <yasser.lamsaougar@gmail.com>
2019-10-07 19:28:43 +0200

    complete Topic Crud

--- web/router.ex
+++ web/router.ex
@@ -16,10 +16,12 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-
+#    get "/topics/new", TopicController, :new
+#    get "/topics/:id/edit", TopicController, :edit
+#    put "/topics/:id", TopicController, :update
+#    post "/topics", TopicController, :create
+#    delete "/topics/:id", TopicController, :delete
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
