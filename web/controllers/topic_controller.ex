3d73d28898f39e5e67577e7e4a07e052175e7275
Lamsaougar <yasser.lamsaougar@gmail.com>
2019-10-07 18:04:09 +0200

    add list of topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,8 +2,27 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
-  def new(conn, params) do
-    changeset = Topic.changeset(%Topic{},%{})
-    render conn, "new.html"
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> render "new.html", changeset: changeset
+    end
   end
 end
